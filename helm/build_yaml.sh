a3896f676c6508cf7248bfa15c0fad24b1aab6ff
cwgdata <cwg100@gmail.com>
2019-07-10 15:26:35 +0100

    readme edits

--- helm/build_yaml.sh
+++ helm/build_yaml.sh
@@ -1,3 +1,5 @@
 PROV=$1
 cp providers/aws.yaml providers/$PROV.yaml
 sed -i '' s/REPLACE_ME/$PROV/g providers/$PROV.yaml
+cp ../README.md ../README-$PROV.md
+sed -i '' s/REPLACE_THIS/$PROV/g ../README-$PROV.md
